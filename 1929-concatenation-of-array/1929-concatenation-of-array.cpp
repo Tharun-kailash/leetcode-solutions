cbae569ac3813abe906059da3232ae0f7a3ff794
Tharun kailash K <tharunkailash2020@gmail.com>
2023-02-10 00:05:19 +0530

    Time: 6 ms (90.75%), Space: 12.5 MB (90.29%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        int i,s=nums.size();
-        for(i=0;i<s;i++){
-            nums.push_back(nums[i]);
+        // int i,s=nums.size();
+        // for(i=0;i<s;i++){
+        //     nums.push_back(nums[i]);
+        // }
+        // return nums;
+        vector<int> newarr(2*nums.size());
+        int i;
+        for(i=0;i<nums.size();i++){
+            newarr[i]=nums[i];
+            newarr[i+nums.size()]=nums[i];
         }
-        return nums;
+        return newarr;
     }
 };
